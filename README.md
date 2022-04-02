2dc6d7a875d23cd5b50574b39c470018bf4e6c86
Daniel Vidal <daniel@ribeirovidal.com.br>
2022-04-02 18:38:30 -0300

    Base do site em node criada

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # nodets-canil
+Projeto feito no módulo do curso Node + Typescript da B7Web
+
+### Pré-Requisitos globais:
+`npm i -g nodemon typescript ts-node`
+
+### Instalação
+`npm install`
+
+### Para rodar o projeto
+`npm run start-dev`
---
+++ src/controllers/pageController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+
+export const home = (req: Request, res: Response)=>{
+
+    //res.sender('pages/page');
+};
+
+export const dogs = (req: Request, res: Response)=>{
+
+    //res.sender('pages/page');
+};
+
+export const cats = (req: Request, res: Response)=>{
+
+    //res.sender('pages/page');
+};
+
+export const fishes = (req: Request, res: Response)=>{
+
+    //res.sender('pages/page');
+};
