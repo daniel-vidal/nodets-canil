bfb36c7ba1b52ae090aae6c86306a2db052ca439
Daniel Vidal <daniel@ribeirovidal.com.br>
2022-04-03 00:10:20 -0300

    Final Project 1.0

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,21 +1,56 @@
 import { Request, Response } from 'express';
 
-export const home = (req: Request, res: Response)=>{
+import { Pet } from '../models/pet';
+import { createMenuObject } from '../helpers/createMenuObjetcs';
 
-    //res.sender('pages/page');
+export const home = (req: Request, res: Response)=>{
+    let list = Pet.getAll();
+
+    res.render('pages/page',{
+        menu: createMenuObject('all'),
+        banner:{
+            tittle: 'Todos os animais',
+            background: 'allanimals.jpg'
+        },
+        list
+    });
 };
 
 export const dogs = (req: Request, res: Response)=>{
+    let list = Pet.getFromType('dog');
 
-    //res.sender('pages/page');
+    res.render('pages/page',{
+        menu: createMenuObject('dog'),
+        banner:{
+            tittle: 'Cachorros',
+            background: 'banner_dog.jpg'
+        },
+        list
+    });
 };
 
 export const cats = (req: Request, res: Response)=>{
+    let list = Pet.getFromType('cat');
 
-    //res.sender('pages/page');
+    res.render('pages/page',{
+        menu: createMenuObject('cat'),
+        banner:{
+            tittle: 'Gatos',
+            background: 'banner_cat.jpg'
+        },
+        list
+    });
 };
 
 export const fishes = (req: Request, res: Response)=>{
+    let list = Pet.getFromType('fish');
 
-    //res.sender('pages/page');
+    res.render('pages/page',{
+        menu: createMenuObject('fish'),
+        banner:{
+            tittle: 'Peixes',
+            background: 'banner_fish.jpg'
+        },
+        list
+    });
 };
